3ebb071bbda98c7666322949f6be69d44d92ab76
Pavel Jarkovský <jarkovsky.pavel@gmail.com>
2021-01-20 09:20:42 +0100

    Updatovaný rok

--- Ticketník/Properties/AssemblyInfo.cs
+++ Ticketník/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ticketník")]
-[assembly: AssemblyCopyright("Copyright © Pavel Jarkovský 2020")]
+[assembly: AssemblyCopyright("Copyright © Pavel Jarkovský 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
