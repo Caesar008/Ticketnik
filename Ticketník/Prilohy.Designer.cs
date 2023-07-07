5822ab34490082a895a8b743e25f800e36d6b39e
Pavel Jarkovský <jarkovsky.pavel@gmail.com>
2023-07-07 10:36:20 +0200

    příprava na přílohy

--- Ticketník/Prilohy.Designer.cs
+++ Ticketník/Prilohy.Designer.cs
@@ -28,20 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new Ticketník.CustomControls.ListView();
+            this.richTextBox1 = new Ticketník.CustomControls.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.delBtn = new Ticketník.CustomControls.Button();
+            this.addBtn = new Ticketník.CustomControls.Button();
+            this.findBtn = new Ticketník.CustomControls.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listView1.HeaderFillMethod = Ticketník.CustomControls.ListView.HeaderTrailingSpaceFill.ExtendLastColumn;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(258, 397);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.richTextBox1.Location = new System.Drawing.Point(276, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = false;
+            this.richTextBox1.SelectionAlignment = System.Windows.Forms.HorizontalAlignment.Left;
+            this.richTextBox1.Size = new System.Drawing.Size(512, 426);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "richTextBox1";
+            this.richTextBox1.WordWrap = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(276, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 426);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // delBtn
+            // 
+            this.delBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.delBtn.Location = new System.Drawing.Point(195, 415);
+            this.delBtn.Name = "delBtn";
+            this.delBtn.Size = new System.Drawing.Size(75, 23);
+            this.delBtn.TabIndex = 3;
+            this.delBtn.Text = "Smazat";
+            this.delBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.addBtn.Location = new System.Drawing.Point(12, 415);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 4;
+            this.addBtn.Text = "Přidat";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // findBtn
+            // 
+            this.findBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.findBtn.Location = new System.Drawing.Point(103, 415);
+            this.findBtn.Name = "findBtn";
+            this.findBtn.Size = new System.Drawing.Size(75, 23);
+            this.findBtn.TabIndex = 5;
+            this.findBtn.Text = "Najít";
+            this.findBtn.UseVisualStyleBackColor = true;
             // 
             // Prilohy
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.findBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.delBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listView1);
             this.Name = "Prilohy";
             this.ShowIcon = false;
             this.Text = "Prilohy";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private CustomControls.ListView listView1;
+        private CustomControls.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private CustomControls.Button delBtn;
+        private CustomControls.Button addBtn;
+        private CustomControls.Button findBtn;
     }
 }
